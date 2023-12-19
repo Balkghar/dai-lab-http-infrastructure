9b700242949a7fade86f3a465c6f41bb68aa695c
acm <a-mango@users.noreply.github.com>
2023-12-19 19:39:07 +0100

    feat: remove unused attributes and add new constructor for Note class

--- api/src/main/java/ch/heig/dai/lab/http/api/Note.java
+++ api/src/main/java/ch/heig/dai/lab/http/api/Note.java
@@ -3,14 +3,11 @@ package ch.heig.dai.lab.http.api;
 import java.time.LocalDateTime;
 
 public class Note {
-    private static int lastId = 0;
-    private int id;
     private String title;
     private String content;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    // Constructor without id, for creating new notes
     public Note(String title, String content) {
         this.title = title;
         this.content = content;
@@ -18,58 +15,27 @@ public class Note {
         this.updatedAt = LocalDateTime.now();
     }
 
-    // Full constructor
-    public Note(int id, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = lastId++;
-        this.title = title;
-        this.content = content;
+    public Note() {
+    }
+
+    public Note(String updatedTitle, String updatedContent, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.title = updatedTitle;
+        this.content = updatedContent;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
 
     // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String title() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String content() {
         return content;
     }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
     @Override
     public String toString() {
-        return "Note{" + "id=" + id + ", title='" + title + '\'' + ", content='" + content + '\'' + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + '}';
+        return "Note{title='" + title + '\'' + ", content='" + content + '\'' + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + '}';
     }
 }
