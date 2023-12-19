06f3363e3c7598837e6b71775c0e273bda9cf386
acm <a-mango@users.noreply.github.com>
2023-12-19 19:51:04 +0100

    rfr: modify Note to record type

--- api/src/main/java/ch/heig/dai/lab/http/api/Note.java
+++ api/src/main/java/ch/heig/dai/lab/http/api/Note.java
@@ -2,40 +2,15 @@ package ch.heig.dai.lab.http.api;
 
 import java.time.LocalDateTime;
 
-public class Note {
-    private String title;
-    private String content;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+public record Note(String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public Note() {
+        this(null, null, null, null);
+    }
 
-    public Note(String title, String content) {
+    public Note(String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.title = title;
         this.content = content;
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
-
-    public Note() {
-    }
-
-    public Note(String updatedTitle, String updatedContent, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.title = updatedTitle;
-        this.content = updatedContent;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
-    }
-
-    // Getters and Setters
-    public String title() {
-        return title;
-    }
-
-    public String content() {
-        return content;
-    }
-
-    @Override
-    public String toString() {
-        return "Note{title='" + title + '\'' + ", content='" + content + '\'' + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + '}';
     }
 }
