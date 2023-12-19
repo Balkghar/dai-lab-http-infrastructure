fa24dd771a5a022f643d2e8063b48aa691fba5fa
acm <a-mango@users.noreply.github.com>
2023-12-19 19:48:41 +0100

    feat: implement CRUD tests for Notes api

--- api/src/test/java/ch/heig/dai/lab/http/api/NoteApiTest.java
+++ api/src/test/java/ch/heig/dai/lab/http/api/NoteApiTest.java
@@ -2,6 +2,7 @@ package ch.heig.dai.lab.http.api;
 
 import io.javalin.http.Context;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -26,31 +27,13 @@ public class NoteApiTest {
         notesService = mock(NotesService.class);
         noteRoutes = new NoteRoutes(notesService); // Use the real NoteRoutes with the mocked service
         ctx = mock(Context.class);
-
-        // Create mock notes as Documents
-        Document doc1 = new Document("title", "title1").append("content", "content1");
-        Document doc2 = new Document("title", "title2").append("content", "content2");
-
-        // Mock the behavior of notesService.createNote()
-//        when(notesService.createNote(any(Note.class))).thenAnswer(invocation -> {
-//            Note note = invocation.getArgument(0);
-//            if (note.title().equals("title1") && note.content().equals("content1")) {
-//                return doc1;
-//            } else if (note.title().equals("title2") && note.content().equals("content2")) {
-//                return doc2;
-//            } else {
-//                return null;
-//            }
-//        });
     }
 
+    // FIXME: extend to test when no notes are present
     @Test
     public void getAllNotes() {
         // Arrange
-        List<Document> expectedDocs = Arrays.asList(
-            new Document("title", "title1").append("content", "content1"),
-            new Document("title", "title2").append("content", "content2")
-        );
+        List<Document> expectedDocs = Arrays.asList(new Document("title", "title1").append("content", "content1"), new Document("title", "title2").append("content", "content2"));
         when(notesService.getAllNotes()).thenReturn(expectedDocs);
 
         // Act
@@ -59,58 +42,94 @@ public class NoteApiTest {
         // Assert
         verify(notesService).getAllNotes();
         verify(ctx).json(expectedDocs);
+        verify(ctx).status(200);
     }
 
     @Test
     public void getNote() {
         // Arrange
-        int id = 1;
-        Document expectedDoc = new Document("title", "title1").append("content", "content1");
-        when(ctx.pathParam("id")).thenReturn(String.valueOf(id));
-        when(notesService.getNote(id)).thenReturn(expectedDoc);
+        String id = new ObjectId().toString(); // Generate a random ObjectId
+        String expectedNote = "{\"_id\": {\"$oid\": \"" + id + "\"}, \"title\": \"title1\", \"content\": \"content1\"}";
+        when(ctx.pathParam("id")).thenReturn(id);
+        when(notesService.getNote(id)).thenReturn(Document.parse(expectedNote));
 
         // Act
         noteRoutes.getNote(ctx);
 
         // Assert
-        verify(notesService).getNoteById(id);
-        verify(ctx).json(expectedDoc);
+        verify(notesService).getNote(id);
+        verify(ctx).json(expectedNote);
+        verify(ctx).status(200);
     }
 
     @Test
     public void createNote() {
         // Arrange
+        Note testNote = new Note(); // Properly initialized Note object
+        Document createdNote = new Document();
 
+        when(ctx.bodyAsClass(Note.class)).thenReturn(testNote);
+        when(notesService.createNote(testNote)).thenReturn(createdNote);
 
         // Act
+        noteRoutes.createNote(ctx);
 
         // Assert
+        verify(notesService).createNote(testNote);
+        verify(ctx).status(201);
+        verify(ctx).json(createdNote);
     }
-
 
     @Test
     public void updateNote() {
+        // Arrange
+        String noteId = "123"; // Example note ID
+        Note updatedNote = new Note("updatedTitle", "updatedContent", null, null); // Assume Note has this constructor
+        Document updatedDocument = new Document();
 
+        when(ctx.pathParam("id")).thenReturn(noteId);
+        when(ctx.bodyAsClass(Note.class)).thenReturn(updatedNote);
+        when(notesService.updateNote(noteId, updatedNote)).thenReturn(updatedDocument);
+
+        // Act
+        noteRoutes.updateNote(ctx);
+
+        // Assert
+        verify(notesService).updateNote(noteId, updatedNote);
+        verify(ctx).status(200); // Verify that status 200 is set
+        verify(ctx).json(updatedDocument); // Verify that the response is JSON
     }
 
     @Test
-    public void deleteNote() {
+    public void deleteNote_SuccessfulDeletion() {
+        // Arrange
+        String noteId = "123";
+        Document deletedNote = new Document(); // Assuming Document is correctly instantiated
+        when(ctx.pathParam("id")).thenReturn(noteId);
+        when(notesService.deleteNote(noteId)).thenReturn(deletedNote);
 
+        // Act
+        noteRoutes.deleteNote(ctx);
+
+        // Assert
+        verify(notesService).deleteNote(noteId);
+        verify(ctx).status(200);
+        verify(ctx).json(deletedNote);
     }
 
-//    @Test
-//    public void testGetNote() {
-//        // Arrange
-//        int id = 1;
-//        Note note = new Note(); // Create a sample note with id 1
-//        when(ctx.pathParam("id")).thenReturn(String.valueOf(id));
-//        when(notesService.getNoteById(id)).thenReturn(note);
-//
-//        // Act
-//        // Call your route handler here, e.g., noteRoutes.getNote(ctx);
-//
-//        // Assert
-//        verify(notesService).getNoteById(id);
-//        verify(ctx).json(note);
-//    }
+    @Test
+    public void deleteNote_NoteNotFound() {
+        // Arrange
+        String noteId = "nonExistingId";
+        when(ctx.pathParam("id")).thenReturn(noteId);
+        when(notesService.deleteNote(noteId)).thenReturn(null);
+
+        // Act
+        noteRoutes.deleteNote(ctx);
+
+        // Assert
+        verify(notesService).deleteNote(noteId);
+        verify(ctx).status(404);
+        verify(ctx).result("Note not found");
+    }
 }
