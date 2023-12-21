f56a5645029dd3987ad32e489f5af343bb255f56
acm <a-mango@users.noreply.github.com>
2023-12-21 16:18:16 +0100

    feat: implement full blog api testing

--- api/src/test/java/ch/heig/dai/lab/http/api/BlogApiTest.java
+++ api/src/test/java/ch/heig/dai/lab/http/api/BlogApiTest.java
@@ -1,5 +1,6 @@
 package ch.heig.dai.lab.http.api;
 
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
 import org.bson.Document;
 import org.bson.types.ObjectId;
@@ -34,37 +35,51 @@ public class BlogApiTest {
 
     @Test
     public void createResource_whenResourceIsValid_addsNewResource() {
-        // Arrange
         Blog testBlog = new Blog();
         Document createdBlog = new Document();
 
         when(ctx.bodyAsClass(Blog.class)).thenReturn(testBlog);
         when(blogService.createBlog(testBlog)).thenReturn(createdBlog);
 
-        // Act
         blogController.create(ctx);
 
-        // Assert
         verify(blogService).createBlog(testBlog);
         verify(ctx).status(201);
         verify(ctx).json(createdBlog);
     }
 
     @Test
-    public void getResource_WhenIdIsValid_ReturnsResource() {
-        // Arrange
+    public void createResource_whenResourceIsInvalid_returnBadRequest() {
+        when(ctx.bodyAsClass(Blog.class)).thenThrow(new BadRequestResponse());
+
+        blogController.create(ctx);
+
+        verify(ctx).status(400);
+    }
+
+    @Test
+    public void getResource_whenIdIsValid_returnsResource() {
         String id = new ObjectId().toString();
         String expectedBlog = "{\"_id\": {\"$oid\": \"" + id + "\"}, \"title\": \"title1\", \"content\": \"content1\"}";
         when(ctx.pathParam("id")).thenReturn(id);
         when(blogService.getBlogById(id)).thenReturn(Document.parse(expectedBlog));
 
-        // Act
         blogController.getOne(ctx, id);
 
-        // Assert
         verify(blogService).getBlogById(id);
         verify(ctx).json(expectedBlog);
         verify(ctx).status(200);
+    }
+
+    @Test
+    public void getResource_whenIdIsInvalid_returnsNotFound() {
+        String id = "invalidId";
+        when(ctx.pathParam("id")).thenReturn(id);
+        when(blogService.getBlogById(id)).thenReturn(null);
+
+        blogController.getOne(ctx, id);
+
+        verify(ctx).status(404);
     }
 
     @Test
@@ -83,6 +98,15 @@ public class BlogApiTest {
     }
 
     @Test
+    public void getAllResources_whenIdIsInvalid_returnsNotFound() {
+        when(blogService.getAllBlogs()).thenReturn(null);
+
+        blogController.getAll(ctx);
+
+        verify(ctx).status(404);
+    }
+
+    @Test
     public void getAllResources_withNoResources_returnsNotFound() {
         // Arrange
         when(blogService.getAllBlogs()).thenReturn(null);
@@ -98,52 +122,66 @@ public class BlogApiTest {
 
     @Test
     public void updateResource_whenResourceExists_updatesResource() {
-        // Arrange
         String blogId = "123";
         Blog updatedBlog = new Blog("updatedTitle", "updatedContent", null, null);
         Document updatedDocument = new Document();
-
         when(ctx.pathParam("id")).thenReturn(blogId);
         when(ctx.bodyAsClass(Blog.class)).thenReturn(updatedBlog);
         when(blogService.updateBlog(blogId, updatedBlog)).thenReturn(updatedDocument);
 
-        // Act
         blogController.update(ctx, blogId);
 
-        // Assert
         verify(blogService).updateBlog(blogId, updatedBlog);
         verify(ctx).status(200); // Verify that status 200 is set
         verify(ctx).json(updatedDocument); // Verify that the response is JSON
     }
 
     @Test
-    public void deleteResource_WhenIdIsValid_RemovesResource() {
-        // Arrange
+    public void updateResource_whenIdIsInvalid_returnsNotFound() {
+        String id = "invalidId";
+        Blog updatedBlog = new Blog("updatedTitle", "updatedContent", null, null);
+        when(ctx.pathParam("id")).thenReturn(id);
+        when(ctx.bodyAsClass(Blog.class)).thenReturn(updatedBlog);
+        when(blogService.updateBlog(id, updatedBlog)).thenReturn(null);
+
+        blogController.update(ctx, id);
+
+        verify(ctx).status(404);
+    }
+
+    @Test
+    public void updateResource_whenResourceIsInvalid_returnsBadRequest() {
+        String id = "123";
+        when(ctx.pathParam("id")).thenReturn(id);
+        when(ctx.bodyAsClass(Blog.class)).thenThrow(new BadRequestResponse());
+
+        blogController.update(ctx, id);
+
+        verify(ctx).status(400);
+    }
+
+    @Test
+    public void deleteResource_whenIdIsValid_removesResource() {
         String blogId = "123";
         Document deletedBlog = new Document();
         when(ctx.pathParam("id")).thenReturn(blogId);
         when(blogService.deleteBlog(blogId)).thenReturn(deletedBlog);
 
-        // Act
         blogController.delete(ctx, blogId);
 
-        // Assert
         verify(blogService).deleteBlog(blogId);
         verify(ctx).status(200);
         verify(ctx).json(deletedBlog);
     }
 
     @Test
-    public void deleteResource_WhenIdIsInvalid_returnsNotFound() {
-        // Arrange
+    public void deleteResource_whenIdIsInvalid_returnsNotFound() {
         String blogId = "nonExistingId";
         when(ctx.pathParam("id")).thenReturn(blogId);
         when(blogService.deleteBlog(blogId)).thenReturn(null);
 
-        // Act
         blogController.delete(ctx, blogId);
 
-        // Assert
         verify(blogService).deleteBlog(blogId);
         verify(ctx).status(404);
         verify(ctx).result("Blog not found");
