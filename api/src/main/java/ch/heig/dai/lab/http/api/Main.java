ea8dc95c695f7fa30dceee83198c903c83a67348
acm <a-mango@users.noreply.github.com>
2024-01-02 15:21:52 +0100

    fix: replace PUT with PATH in access control allowed methods

--- api/src/main/java/ch/heig/dai/lab/http/api/Main.java
+++ api/src/main/java/ch/heig/dai/lab/http/api/Main.java
@@ -29,7 +29,7 @@ public class Main {
 
         // Enable CORS for all requests
         app.before(ctx -> ctx.header("Access-Control-Allow-Origin", "*")
-                             .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"));
+                             .header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"));
 
 
         // Register error handler
